28257bcf2cd6f6f93cbaf1642c7850f14954df3e
Austin Abild <abildae25@mounties.mansfield.edu>
2017-01-31 08:55:48 -0500

    Comments

---
+++ src/hello2/Hello2.java
@@ -0,0 +1,15 @@
+/*
+ * Author:      Austin Abild
+ * Program:     Hello 2
+ * Purpose:     Practice using github program
+ * Date:        1/31/17
+ */
+package hello2;
+
+public class Hello2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello 2");
+    }
+    
+}
